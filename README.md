973e0d6aed238f472be95727020e68e4e908777f
Aseelsamer <assoul_08@hotmail.com>
2020-11-16 16:36:23 +0200

    add

--- README.md
+++ README.md
@@ -1,17 +1,34 @@
-Number and name of feature: #1 Display Image
-Estimate of time needed to complete : one hour and half
-start time:2:15pm
-finish time:2:50PM
+Number and name of feature: #1 Display Image 
+
+Estimate of time needed to complete : one hour and half 
+
+start time:2:15pm 
+
+finish time:2:50PM 
+
 Actual time needed to complete:40 minutes
 
-Number and name of feature: #2 Filter images
-Estimate of time needed to complete :one and half hour
-start time:2:50 pm 
-finish time:
-Actual time needed to complete:
 
-Number and name of feature: 
-Estimate of time needed to complete :
-start time:
-finish time:
-Actual time needed to complete:
+
+Number and name of feature: #2 Filter images
+
+Estimate of time needed to complete : one hour and half 
+
+start time: 2:50 PM
+
+finish time: 3:35 PM 
+
+Actual time needed to complete: 45 minutes
+
+
+Number and name of feature: #3 Style the application
+
+Estimate of time needed to complete : one hour and half 
+
+start time: 3:35 PM
+
+finish time: 4:20 PM 
+
+Actual time needed to complete: 45 minutes. 
+
+
--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 'use strict';
+let imagesArray = [];
 function Image(url,title,description,keyword,horns){
   this.url=url;
   this.title=title;
   this.description=description;
   this.keyword=keyword;
   this.horns=horns;
+  imagesArray.push(this);
 }
 
 $.ajax('../data/page-1.json')
@@ -14,12 +16,16 @@ $.ajax('../data/page-1.json')
       let image= new Image(val.image_url,val.title,val.description,val.keyword,val.horns);
       //   console.log(image);
       image.render();
+
     });
+    $('#photo-template').first().remove();
+    renderSelect();
   });
 
 
 Image.prototype.render = function(){
   let imageTemplate= $('#photo-template').first().clone();
+  imageTemplate.attr('id',null);
   imageTemplate.find('h2').text(this.title);
 
   imageTemplate.find('img').attr('src',this.url);
@@ -29,3 +35,39 @@ Image.prototype.render = function(){
 
   console.log(imageTemplate.html());
 };
+
+let select = $('select');
+
+
+const renderSelect =()=>{
+  let arr=[];
+  imagesArray.forEach(element => {
+    if( ! arr.includes(element.keyword) ){
+      arr.push(element.keyword);
+      select.append(`<option> ${element.keyword} </option> `);
+
+    }
+  });
+  console.log(imagesArray, ' \n after filtering \n ' , arr);
+};
+
+select.on('change', function(){
+  let keyword = this.value;
+
+  $('main').html('');
+  imagesArray.forEach(element => {
+
+    if(element.keyword === keyword){
+      let image = $(`
+           <section>
+           <h2>${element.title}</h2>
+           <img src=${element.url} alt="">
+           <p>${element.description}</p>
+         </section>`) ;
+      $('main').append(image);
+
+    }
+  });
+
+
+});
