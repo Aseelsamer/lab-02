1af3f6f345a804fe2fd83737072c134bbca29039
Aseelsamer <assoul_08@hotmail.com>
2020-11-17 09:00:45 +0200

    add

--- js/app.js
+++ js/app.js
@@ -9,14 +9,14 @@ function Image(url,title,description,keyword,horns){
   imagesArray.push(this);
 }
 
-$.ajax('../data/page-1.json')
+$.ajax('./data/page-1.json')
   .then(data=>{
     // console.log(data);
     data.forEach((val)=>{
       let image= new Image(val.image_url,val.title,val.description,val.keyword,val.horns);
       //   console.log(image);
       image.render();
-      
+
     });
     $('#photo-template').first().remove();
     renderSelect();
@@ -40,34 +40,34 @@ let select = $('select');
 
 
 const renderSelect =()=>{
-    let arr=[];
-    imagesArray.forEach(element => {
-        if( ! arr.includes(element.keyword) ){  
-            arr.push(element.keyword);
-            select.append(`<option> ${element.keyword} </option> `);
+  let arr=[];
+  imagesArray.forEach(element => {
+    if( ! arr.includes(element.keyword) ){
+      arr.push(element.keyword);
+      select.append(`<option> ${element.keyword} </option> `);
 
-        }
-    });
-    console.log(imagesArray, " \n after filtering \n " , arr);
-}
+    }
+  });
+  console.log(imagesArray, ' \n after filtering \n ' , arr);
+};
 
 select.on('change', function(){
-    let keyword = this.value;
+  let keyword = this.value;
 
-    $('main').html('');
-    imagesArray.forEach(element => {
-        
-        if(element.keyword === keyword){
-           let image = $(`
+  $('main').html('');
+  imagesArray.forEach(element => {
+
+    if(element.keyword === keyword){
+      let image = $(`
            <section>
            <h2>${element.title}</h2>
            <img src=${element.url} alt="">
            <p>${element.description}</p>
          </section>`) ;
-         $('main').append(image);
-         
-        }
-    });
-    
+      $('main').append(image);
 
-})
+    }
+  });
+
+
+});
